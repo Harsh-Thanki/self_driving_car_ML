a505eac2a49b649cdee785b8b8922f565d1d776b
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-05-30 08:58:46 +0530

    obj of class Road created and method call added.

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ canvas.width = 200;
 // Creating 2D context to draw
 const ctx = canvas.getContext("2d");
 
+// Object of class Road
+const road = new Road(canvas.width/2,canvas.width*0.9);
+
 // Object of class Car
 const car = new Car(100, 100, 30, 50);
 
@@ -14,6 +17,8 @@ function animate()
 {
     car.update();
     canvas.height = window.innerHeight;
+    // Calling draw() of class Road
+    road.draw(ctx);
     // Calling draw() of class Car
     car.draw(ctx);
     requestAnimationFrame(animate);
