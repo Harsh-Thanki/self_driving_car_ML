2686b18651c2be78d3995c96ffddb5666da7d133
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-05-30 12:27:37 +0530

    getLaneCenter() call added.

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2,canvas.width*0.9);
 
 // Object of class Car
-const car = new Car(100, 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
