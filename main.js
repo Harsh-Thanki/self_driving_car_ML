2d8cb240dd075348d9d104df50aa6208c2650871
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-05-30 22:32:35 +0530

    constructor calls updated & update and draw loops added.

--- main.js
+++ main.js
@@ -9,12 +9,20 @@ const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2,canvas.width*0.9);
 
 // Object of class Car
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+
+const traffic=[
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY",2)
+];
 
 animate();
 
 function animate()
 {
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].update(road.borders);
+    }
+
     car.update(road.borders);
     canvas.height = window.innerHeight;
 
@@ -23,6 +31,11 @@ function animate()
 
     // Calling draw() of class Road
     road.draw(ctx);
+
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(ctx);
+    }
+
     // Calling draw() of class Car
     car.draw(ctx);
 
