b9dfcde0e54367852872cda71e5086034b1a2ea5
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-05-31 14:10:07 +0530

    argumments updated.

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2,canvas.width*0.9);
 
 // Object of class Car
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 
 const traffic=[
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY",2)
