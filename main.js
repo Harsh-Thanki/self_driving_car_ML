cdb3703f04c6894210fd24b1d12fcddafe3be360
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-05-31 19:07:39 +0530

    bestCar stored & other reformating.

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
-// Positioning the canvas
-const carCanvas = document.getElementById("carCanvas");
-carCanvas.width = 200;
-const networkCanvas = document.getElementById("networkCanvas");
-networkCanvas.width = 300;
+const carCanvas=document.getElementById("carCanvas");
+carCanvas.width=200;
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=300;
 
-// Creating 2D context to draw
 const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
-// Object of class Road
-const road = new Road(carCanvas.width/2,carCanvas.width*0.9);
+const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 
-const N=100;
+const N=1;
 const cars=generateCars(N);
+let bestCar=cars[0];
+if(localStorage.getItem("bestBrain")){
+    for(let i=0;i<cars.length;i++){
+        cars[i].brain=JSON.parse(
+            localStorage.getItem("bestBrain"));
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain,0.1);
+        }
+    }
+}
 
 const traffic=[
-    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY",2)
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-700,30,50,"DUMMY",2),
 ];
 
 animate();
+
+function save(){
+    localStorage.setItem("bestBrain",
+        JSON.stringify(bestCar.brain));
+}
+
+function discard(){
+    localStorage.removeItem("bestBrain");
+}
 
 function generateCars(N){
     const cars=[];
@@ -28,12 +50,10 @@ function generateCars(N){
     return cars;
 }
 
-function animate(time)
-{
+function animate(time){
     for(let i=0;i<traffic.length;i++){
         traffic[i].update(road.borders,[]);
     }
-
     for(let i=0;i<cars.length;i++){
         cars[i].update(road.borders,traffic);
     }
@@ -42,15 +62,13 @@ function animate(time)
             ...cars.map(c=>c.y)
         ));
 
-    carCanvas.height = window.innerHeight;
-    networkCanvas.height = window.innerHeight;
+    carCanvas.height=window.innerHeight;
+    networkCanvas.height=window.innerHeight;
 
     carCtx.save();
-    carCtx.translate(0,-bestCar.y+carCanvas.height*0.7)
+    carCtx.translate(0,-bestCar.y+carCanvas.height*0.7);
 
-    // Calling draw() of class Road
     road.draw(carCtx);
-
     for(let i=0;i<traffic.length;i++){
         traffic[i].draw(carCtx,"red");
     }
