d7bee0e661aecc93677f6fdedf74b1b338a05f4e
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-05-31 14:19:09 +0530

    canvas renamed to carCanvas & MyCanvas changed to carCanvas.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Positioning the canvas
-const canvas = document.getElementById("MyCanvas");
-canvas.width = 200;
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
 
 // Creating 2D context to draw
-const ctx = canvas.getContext("2d");
+const ctx = carCanvas.getContext("2d");
 
 // Object of class Road
-const road = new Road(canvas.width/2,canvas.width*0.9);
+const road = new Road(carCanvas.width/2,carCanvas.width*0.9);
 
 // Object of class Car
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
@@ -24,10 +24,10 @@ function animate()
     }
 
     car.update(road.borders,traffic);
-    canvas.height = window.innerHeight;
+    carCanvas.height = window.innerHeight;
 
     ctx.save();
-    ctx.translate(0,-car.y+canvas.height*0.7)
+    ctx.translate(0,-car.y+carCanvas.height*0.7)
 
     // Calling draw() of class Road
     road.draw(ctx);
